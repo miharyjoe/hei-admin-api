e576d3df05326f47086b987cb232a6c7fe02e33b
njaina <hei.njaina.2@gmail.com>
2023-03-24 05:10:14 +0300

    feat: update

--- src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
@@ -1,2 +1,47 @@
-package school.hei.haapi.endpoint.rest.controller;public class CourseController {
+package school.hei.haapi.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.endpoint.rest.mapper.CourseMapper;
+//import school.hei.haapi.endpoint.rest.model.Course;
+import school.hei.haapi.model.BoundedPageSize;
+
+import school.hei.haapi.model.PageFromOne;
+import school.hei.haapi.service.CourseService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import school.hei.haapi.endpoint.rest.model.CrupdateCourse;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+@RestController
+@AllArgsConstructor
+public class CourseController {
+    private final CourseService courseService;
+    private final CourseMapper courseMapper;
+
+/**@GetMapping("/courses")
+    public List<Course> getAllCourse (
+            @RequestParam(required = false, defaultValue="1") PageFromOne page,
+            @RequestParam(required = false,name = "page_size",defaultValue = "15") BoundedPageSize pageSize
+    ) {
+        return courseService.getAll(page, pageSize)
+                .stream().map(courseMapper::toRestCourse)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @PutMapping(value = "/courses")
+    public List<Course> createOrUpdateCourses(@RequestBody List<CrupdateCourse> toWrite) {
+        var saved = courseService.saveAll(toWrite.stream()
+                .map(courseMapper::toDomain)
+                .collect(toUnmodifiableList()));
+        return saved.stream()
+                .map(courseMapper::toRest)
+                .collect(toUnmodifiableList());
+    }*/
 }
--- src/main/java/school/hei/haapi/repository/CourseStudentRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseStudentRepository.java
@@ -1,2 +1,18 @@
-package school.hei.haapi.repository;public interface CourseStudentRepository {
+package school.hei.haapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+//import school.hei.haapi.endpoint.rest.model.CourseStatus;
+import school.hei.haapi.model.Course;
+import school.hei.haapi.model.CourseStudent;
+import school.hei.haapi.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CourseStudentRepository extends JpaRepository<CourseStudent, String> {
+//    List<CourseStudent> getAllByStudentIdAndStatus(String student_id, CourseStatus status);
+    List<CourseStudent> findAllByStudent(User student);
+    Optional<CourseStudent> findByStudentAndCourse(User student, Course course);
 }
