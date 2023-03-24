6d8788d71da0b8b53a8f458e5dfed53e66de3bee
njaina <hei.njaina.2@gmail.com>
2023-03-24 05:05:24 +0300

    feat: update service

--- src/main/java/school/hei/haapi/service/CourseService.java
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -1,2 +1,67 @@
-package school.hei.haapi.service;public class CourseService {
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.*;
+//import school.hei.haapi.endpoint.rest.model.CourseStatus;
+import school.hei.haapi.repository.CourseRepository;
+import school.hei.haapi.repository.CourseStudentRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CourseService {
+    private final CourseRepository repository;
+    private final CourseStudentRepository courseStudentRepository;
+    private final UserService userService;
+    public List<Course> getAll(PageFromOne page , BoundedPageSize pageSize){
+        Pageable pageable =
+                PageRequest.of(page.getValue(), pageSize.getValue());
+        return repository.findAll(pageable).toList();
+    }
+
+    public List<Course> saveAll(List<Course> courses) {
+        return repository.saveAll(courses);
+    }
+    public List<Course> findCoursesByStudent(String studentId) {
+        User student = userService.getById(studentId);
+        return courseStudentRepository.findAllByStudent(student).stream()
+                .map(CourseStudent::getCourse)
+                .collect(Collectors.toList());
+    }
+    public List<Course> getCoursesOrderByCodeAsc() {
+        return repository.findAllByOrderByCodeAscIdAsc();
+    }
+    public List<Course> getCoursesOrderByCodeDesc() {
+        return repository.findAllByOrderByCodeDescIdDesc();
+    }
+    public List<Course> getCoursesOrderByCreditsAsc() {
+        return repository.findAllByOrderByCreditsAscIdAsc();
+    }
+    public List<Course> getCoursesOrderByCreditsDesc() {
+        return repository.findAllByOrderByCreditsDescIdDDesc();
+    }
+        public List<Course> getCoursesByTeacherFirstName(String firstName) {
+        return repository.findByMainTeacherFirstNameContainingIgnoreCase(firstName);
+    }
+    public List<Course> getCoursesByTeacherLastName(String lastName){
+        return repository.findByMainTeacherLastNameContainingIgnoreCase(lastName);
+    }
+
+  /**  public void updateCourseStudentStatus(String studentId, String courseId, CourseStatus newStatus) {
+        User student = userService.getById(studentId);
+        Course course = repository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid course id: " + courseId));
+        CourseStudent courseStudent = courseStudentRepository.findByStudentAndCourse(student, course)
+                .orElseThrow(() -> new IllegalArgumentException("No course student found for student " + studentId + " and course " + courseId));
+        courseStudent.setStatus(newStatus);
+        courseStudentRepository.save(courseStudent);
+    }*/
+
+
+
 }
