81a6edbd83f851a09b66186677e7764650213cbb
njaina <hei.njaina.2@gmail.com>
2023-03-24 13:34:04 +0300

    feat: update repository

--- src/main/java/school/hei/haapi/repository/CourseRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseRepository.java
@@ -1,5 +1,6 @@
 package school.hei.haapi.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Course;
@@ -8,11 +9,9 @@ import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
-    List<Course> findAllByOrderByCodeAscIdAsc();
-    List<Course> findAllByOrderByCodeDescIdDesc();
-
-    List<Course> findAllByOrderByCreditsAscIdAsc();
-    List<Course> findAllByOrderByCreditsDescIdDesc();
-    List<Course> findAllByMainTeacherFirsNameContainingIgnoreCase(String firstname);
-    List<Course> findAllByMainTeacherLastNameContainingIgnoreCase(String lastname);
+    List<Course> findAllByCodeContainingIgnoreCaseOrMainTeacherLastNameContainingIgnoreCase(String code, String lastname, Sort sort);
+    List<Course> findAllByCodeContainingIgnoreCase(String code,Sort sort);
+    List<Course> findAllByMainTeacherFirstNameContainingIgnoreCase(String firstname,Sort sort);
+    List<Course> findAllByMainTeacherLastNameContainingIgnoreCase(String lastname,Sort sort);
+    List<Course> findAllByCodeContainingIgnoreCaseAndMainTeacherLastNameContainingIgnoreCase(String code, String lastname,Sort sort);
 }
